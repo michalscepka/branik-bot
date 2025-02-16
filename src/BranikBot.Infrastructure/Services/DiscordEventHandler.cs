0f24e0fdaab61991620aeac57212fe11a4949a4b
Michal Ščepka <55493644+michalscepka@users.noreply.github.com>
2025-02-16 01:12:52 +0100

    Add more response variants

--- src/BranikBot.Infrastructure/Services/DiscordEventHandler.cs
+++ src/BranikBot.Infrastructure/Services/DiscordEventHandler.cs
@@ -65,22 +65,86 @@ public partial class DiscordEventHandler : BackgroundService
             {
                 amount = double.Parse(match.Groups[nameof(PriceGroup.Millions)].Value) * 1_000_000;
             }
-
+            
             var branikCount = (int)(amount / BranikPrice);
-            var bottleFormatted = SetBottleWordFormat(branikCount);
-            chatMessage.AppendLine($"\nTo by stacilo na {branikCount} {bottleFormatted} Branicka ve sleve!\n");
+
+            if (branikCount <= 0)
+            {
+                chatMessage.AppendLine("\nJe mi to lito, ale to neni ani na jednu dvoulitrovku Branika ve sleve.\n");
+                continue;
+            }
+
+            var parcelCount = branikCount / 6;
+            var palletsCount = parcelCount / 100;
+            var trucksCount = palletsCount / 34;
+            var bottlesWord = GetBottleWordFormat(branikCount);
+            var parcelsWord = GetParcelssWordFormat(parcelCount);
+            var palletsWord = GetPalletsWordFormat(palletsCount);
+            var trucksWord = GetTrucksWordFormat(palletsCount);
+            
+            switch (branikCount)
+            {
+                case < 100:
+                    chatMessage.AppendLine($"\nTo by stacilo na {branikCount} {bottlesWord} Branicka ve sleve!\n");
+                    break;
+                case < 1000:
+                {
+                    chatMessage.AppendLine($"\nTo by stacilo na {parcelCount} {parcelsWord} {bottlesWord} Branicka ve sleve!\n");
+                    break;
+                }
+                case < 100_000:
+                {
+                    chatMessage.AppendLine($"\nTo by stacilo na vic jak {palletsCount} euro{palletsWord} ({parcelCount} {parcelsWord}) {bottlesWord} Branicka ve sleve!\n");
+                    break;
+                }
+                case >= 100_000:
+                {
+                    chatMessage.AppendLine($"\nTo by stacilo na {trucksCount} {trucksWord} ({palletsCount} {palletsWord}) {bottlesWord} Branicka ve sleve!\n");
+                    break;
+                }
+            }
         }
 
         await _gatewayClient.Rest.SendMessageAsync(message.ChannelId, chatMessage.ToString());
     }
 
-    private static string SetBottleWordFormat(int branikCount)
+    private static string GetBottleWordFormat(int branikCount)
     {
         return branikCount switch
         {
             1 => "dvoulitrovku",
             >= 2 and <= 4 => "dvoulitrovky",
             _ => "dvoulitrovek"
+        };
+    }
+    
+    private static string GetParcelssWordFormat(int parcelsCount)
+    {
+        return parcelsCount switch
+        {
+            1 => "balik",
+            >= 2 and <= 4 => "baliky",
+            _ => "baliku"
+        };
+    }
+    
+    private static string GetPalletsWordFormat(int palletsCount)
+    {
+        return palletsCount switch
+        {
+            1 => "paletu",
+            >= 2 and <= 4 => "palety",
+            _ => "palet"
+        };
+    }
+    
+    private static string GetTrucksWordFormat(int trucksCount)
+    {
+        return trucksCount switch
+        {
+            1 => "kamion",
+            >= 2 and <= 4 => "kamiony",
+            _ => "kamionu"
         };
     }
 
