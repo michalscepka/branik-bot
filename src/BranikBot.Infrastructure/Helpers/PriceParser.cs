2b5a1ed98e7aca98f80012a3bac57e7fe5469100
michalscepka <55493644+michalscepka@users.noreply.github.com>
2026-01-10 00:57:56 +0100

    Update parser

--- src/BranikBot.Infrastructure/Helpers/PriceParser.cs
+++ src/BranikBot.Infrastructure/Helpers/PriceParser.cs
@@ -1,3 +1,4 @@
+using System.Globalization;
 using System.Text.RegularExpressions;
 using BranikBot.Infrastructure.Enums;
 
@@ -6,7 +7,7 @@ namespace BranikBot.Infrastructure.Helpers;
 public static class PriceParser
 {
     private const string Pattern =
-        @"(?<Ones>\d{1,3}(\s?\d{3})*([.,]\d+)?)\s*(kc|kč|,-|czk|korun|koruny|koruna)\b|(?<Thousands>\d{1,3}(\s?\d{3})*([.,]\d+)?)\s*k\b|(?<Millions>\d{1,3}(\s?\d{3})*([.,]\d+)?)\s*mega\b";
+        @"(?<Ones>\d{1,3}(\s?\d{3})*([.,]\d+)?)\s*((kc|kč|czk|korun|koruny|koruna)\b|,-)|(?<Thousands>\d{1,3}(\s?\d{3})*([.,]\d+)?)\s*k\b|(?<Millions>\d{1,3}(\s?\d{3})*([.,]\d+)?)\s*mega\b";
 
     private static readonly Regex BranikRegex = new (Pattern, RegexOptions.IgnoreCase | RegexOptions.Compiled, TimeSpan.FromSeconds(1));
 
@@ -24,15 +25,21 @@ public static class PriceParser
             var originalValue = match.Groups[0].Value;
 
             if (match.Groups[nameof(PriceGroup.Ones)].Success)
-                result.TryAdd(decimal.Parse(match.Groups[nameof(PriceGroup.Ones)].Value), originalValue);
+                result.TryAdd(ParseDecimal(match.Groups[nameof(PriceGroup.Ones)].Value), originalValue);
 
             if (match.Groups[nameof(PriceGroup.Thousands)].Success)
-                result.TryAdd(decimal.Parse(match.Groups[nameof(PriceGroup.Thousands)].Value) * 1_000, originalValue);
+                result.TryAdd(ParseDecimal(match.Groups[nameof(PriceGroup.Thousands)].Value) * 1_000, originalValue);
 
             if (match.Groups[nameof(PriceGroup.Millions)].Success)
-                result.TryAdd(decimal.Parse(match.Groups[nameof(PriceGroup.Millions)].Value) * 1_000_000, originalValue);
+                result.TryAdd(ParseDecimal(match.Groups[nameof(PriceGroup.Millions)].Value) * 1_000_000, originalValue);
         }
 
         return result;
+    }
+
+    private static decimal ParseDecimal(string value)
+    {
+        var normalized = value.Replace(" ", "").Replace(",", ".");
+        return decimal.Parse(normalized, CultureInfo.InvariantCulture);
     }
 }
